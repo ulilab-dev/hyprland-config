ab1e728bc9bda50118cebd141bd9fcb88a6fed14
ulilab-dev <ullia5733@gmail.com>
2026-05-18 23:40:45 +0700

    Update env.lua

--- hypr/custom/env.lua
+++ hypr/custom/env.lua
@@ -3,6 +3,7 @@
 -------------------------------
 
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
+
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
 hl.env("XDG_SESSION_TYPE", "wayland")
 hl.env("XDG_DESKTOP_PORTAL", "hyprland")
