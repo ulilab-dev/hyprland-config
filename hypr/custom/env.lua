6c5b33c794bdf58acaea9b0921c4e45466d607e1
ulilab-dev <ullia5733@gmail.com>
2026-05-18 23:39:35 +0700

    Add environment variables for Hyprland configuration

--- hypr/custom/env.lua
+++ hypr/custom/env.lua
@@ -3,8 +3,13 @@
 -------------------------------
 
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_DESKTOP_PORTAL", "hyprland")
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
 
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
