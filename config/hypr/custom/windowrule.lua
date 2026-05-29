a16e583a6ce95626d98ebcf4bcbeb15f1c41ad70
ulilab-dev <ullia5733@gmail.com>
2026-05-29 14:17:23 +0700

    Add files via upload

---
+++ config/hypr/custom/windowrule.lua
@@ -0,0 +1,54 @@
+--------------------------------
+---- WINDOWS AND WORKSPACES ----
+--------------------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Window-Rules/
+-- and https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
+
+-- Example window rules that are useful
+
+local suppressMaximizeRule = hl.window_rule({
+    -- Ignore maximize requests from all apps. You'll probably like this.
+    name  = "suppress-maximize-events",
+    match = { class = ".*" },
+
+    suppress_event = "maximize",
+})
+-- suppressMaximizeRule:set_enabled(false)
+
+hl.window_rule({
+    -- Fix some dragging issues with XWayland
+    name  = "fix-xwayland-drags",
+    match = {
+        class      = "^$",
+        title      = "^$",
+        xwayland   = true,
+        float      = true,
+        fullscreen = false,
+        pin        = false,
+    },
+
+    no_focus = true,
+})
+
+-- Layer rules also return a handle.
+-- local overlayLayerRule = hl.layer_rule({
+--     name  = "no-anim-overlay",
+--     match = { namespace = "^my-overlay$" },
+--     no_anim = true,
+-- })
+-- overlayLayerRule:set_enabled(false)
+
+-- Hyprland-run windowrule
+hl.window_rule({
+    name  = "move-hyprland-run",
+    match = { class = "hyprland-run" },
+
+    move  = "20 monitor_h-120",
+    float = true,
+})
+
+hl.window_rule({ match = { class = "org.pulseaudio.pavucontrol" }, float = true, size = "900 600", })
+hl.window_rule({ match = { class = "localsend" }, float = true, size = "900 600", })
+hl.window_rule({ match = { class = "org.gnome.baobab" }, float = true, size = "900 600", })
+-- hl.window_rule({ match = { class = "steam" }, float = true, size = "1400 800", })
