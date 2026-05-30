955cb6000d541b018517fed9220818dcb9f034e7
ulilab-dev <ullia5733@gmail.com>
2026-05-30 10:03:29 +0700

    Update install.sh

--- config/install.sh
+++ config/install.sh
@@ -1,1 +1,1 @@
-cp -r fish/ gtk-3.0/ gtk-4.0 hypr/ kitty/ nvim/ rofi/ waybar/ wlogout/ ~/.config && echo "DONE"
+cp -r fish/ gtk-3.0/ gtk-4.0/ hypr/ kitty/ nvim/ rofi/ waybar/ wlogout/ ~/.config && echo "DONE"
