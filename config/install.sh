a95564a2f2b1d2fc10a6949a3ebf9dc020af4cc4
ulilab-dev <ullia5733@gmail.com>
2026-05-30 19:35:38 +0700

    Update install.sh

--- config/install.sh
+++ config/install.sh
@@ -3,7 +3,7 @@
 chmod +x ~/hyprland-config/config/waybar/start.sh &&
 chmod +x ~/hyprland-config/config/hypr/wallpaper-pick.sh &&
 
-cp -r fish/ hypr/ kitty/ rofi/ waybar/ wlogout/ ~/.config && cp -r Bibata-Modern-Ice ~/.local/share/icons &
+cp -r fish/ hypr/ kitty/ rofi/ waybar/ wlogout/ ~/.config && cp -r Bibata-Modern-Ice/ ~/.local/share/icons &
 
 if [ $? -eq 0 ]; then
     echo "Done []"
