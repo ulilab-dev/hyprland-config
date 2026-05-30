4c2aecf3226a9e0384650ca6e6a8936c1566c8a0
ulilab-dev <ullia5733@gmail.com>
2026-05-30 19:20:18 +0700

    Update autostart.lua

--- config/hypr/custom/autostart.lua
+++ config/hypr/custom/autostart.lua
@@ -8,6 +8,8 @@
 -- Or execute your favorite apps at launch like this:
 
  hl.on("hyprland.start", function () 
+     hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice'")
+     hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-size 24")
      hl.exec_cmd("nm-applet")
      hl.exec_cmd("waybar")
      hl.exec_cmd("swaybg -m fill -i ~/.config/hypr/wallpapers/p3.jpg")
