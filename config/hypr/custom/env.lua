9b1db0ec55023e3e59a3f7e518095d8c4aa6a85d
ulilab-dev <ullia5733@gmail.com>
2026-05-30 19:00:06 +0700

    Update env.lua

--- config/hypr/custom/env.lua
+++ config/hypr/custom/env.lua
@@ -8,7 +8,6 @@ hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
 hl.env("XDG_SESSION_TYPE", "wayland")
 hl.env("XDG_DESKTOP_PORTAL", "hyprland")
 
-hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Ice")
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
 
