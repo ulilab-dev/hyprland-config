638b73ab86e5474709eb06059e2536b49bf1b1d7
ulilab-dev <ullia5733@gmail.com>
2026-05-30 11:54:41 +0700

    Update install.sh

--- config/install.sh
+++ config/install.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-sudo cp -r fish/ gtk-3.0/ gtk-4.0/ hypr/ kitty/ nvim/ rofi/ waybar/ wlogout/ ~/.config && echo "DONE []" &
 
-chmod +x ~/hyprland-config/config/install.sh &&
-chmod +x ~/hyprland-config/config/waybar/start.sh &&
-chmod +x ~/hyprland-config/config/hypr/wallpaper-pick.sh &&
- 
+chmod +x ~/hyprland-config/config/install.sh
+chmod +x ~/hyprland-config/config/waybar/start.sh
+chmod +x ~/hyprland-config/config/hypr/wallpaper-pick.sh
+
+cp -r fish/ gtk-3.0/ gtk-4.0/ hypr/ kitty/ nvim/ rofi/ waybar/ wlogout/ ~/.config
+
+if [ $? -eq 0 ]; then
+    echo "Done []"
+else
+    echo "Meh! []"
+fi
