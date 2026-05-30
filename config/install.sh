bfb86e209b676b43a0ef601a96f5d9de0cd12c49
ulilab-dev <ullia5733@gmail.com>
2026-05-30 12:17:38 +0700

    Update install.sh

--- config/install.sh
+++ config/install.sh
@@ -6,7 +6,7 @@ chmod +x ~/hyprland-config/config/hypr/wallpaper-pick.sh &&
 
 sudo cp -r fish/ gtk-3.0/ gtk-4.0/ hypr/ kitty/ nvim/ rofi/ waybar/ wlogout/ ~/.config &&
 
-sudo chown -R $USER:$USER ~/.config/gtk-3.0 ~/.config/gtk-4.0 
+sudo chown -R $USER:$USER ~/.config/gtk-3.0 ~/.config/gtk-4.0 &
 
 if [ $? -eq 0 ]; then
     echo "Done []"
