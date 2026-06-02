07031ba6d871ced28c192a749aa86decc4660fcc
ulilab-dev <ullia5733@gmail.com>
2026-06-02 23:26:43 +0700

    Update install.sh

--- config/install.sh
+++ config/install.sh
@@ -3,7 +3,7 @@
 chmod +x waybar/toggle_waybar.sh &&
 chmod +x hypr/wallpaper-pick.sh &&
 
-cp -r fastfetch/ fish/ hypr/ kitty/ rofi/ waybar/ wlogout/ ~/.config && 
+cp -r dunst/ fastfetch/ fish/ hypr/ kitty/ rofi/ waybar/ wlogout/ ~/.config && 
 cp -r Bibata-Modern-Ice/ ~/.local/share/icons &
 
 if [ $? -eq 0 ]; then
