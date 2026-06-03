b78a940331ea4d1480957838d7baee0da51d7702
ulilab-dev <ullia5733@gmail.com>
2026-06-03 18:54:06 +0700

    Update install.sh

--- config/install.sh
+++ config/install.sh
@@ -4,7 +4,7 @@ chmod +x waybar/toggle_waybar.sh &&
 chmod +x hypr/wallpaper-pick.sh &&
 
 cp -r dunst/ fastfetch/ fish/ hypr/ kitty/ rofi/ waybar/ wlogout/ ~/.config && 
-cp -r Bibata-Modern-Ice/ ~/.local/share/icons &
+cp -r Simp1e/ ~/.local/share/icons &
 
 if [ $? -eq 0 ]; then
     echo "Done []"
