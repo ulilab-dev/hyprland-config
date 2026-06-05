2d40f90bb6897a6195ed00bc32a4a0269d37ce73
ulilab-dev <ullia5733@gmail.com>
2026-06-05 12:38:11 +0700

    Update autostart.lua

--- config/hypr/custom/autostart.lua
+++ config/hypr/custom/autostart.lua
@@ -8,7 +8,6 @@
 -- Or execute your favorite apps at launch like this:
 
  hl.on("hyprland.start", function ()
-     hl.exec_cmd("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'")
      hl.exec_cmd("nm-applet")
      hl.exec_cmd("waybar")
      hl.exec_cmd("swaybg -m fill -i ~/.config/hypr/wallpapers/p2.png")
