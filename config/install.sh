60473d27a0559e44c3f3410382274010934714bf
ulilab-dev <ullia5733@gmail.com>
2026-06-06 23:07:06 +0700

    Update install.sh

--- config/install.sh
+++ config/install.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-chmod +x waybar/toggle_waybar.sh &&
-chmod +x hypr/wallpaper-pick.sh &&
-
 cp -r dunst/ fastfetch/ fish/ hypr/ kitty/ rofi/ waybar/ wlogout/ ~/.config && \
 cp -r Simp1e/ ~/.local/share/icons 
 
