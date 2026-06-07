51f19da6e8b706e6ddedb3b709dd9e88070fcb4f
ulilab-dev <ullia5733@gmail.com>
2026-06-07 21:55:41 +0700

    Update keybinds.lua

--- config/hypr/custom/keybinds.lua
+++ config/hypr/custom/keybinds.lua
@@ -21,7 +21,7 @@ hl.bind("CTRL+ALT+T", hl.dsp.exec_cmd(terminal))
 local closeWindowBind = hl.bind(mainMod .. " + Q", hl.dsp.window.close())
 -- closeWindowBind:set_enabled(false)
 hl.bind(mainMod .. " + M",
-  hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
+hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(superMod .. " + F", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(mainMod .. " + D", hl.dsp.exec_cmd(menu))
