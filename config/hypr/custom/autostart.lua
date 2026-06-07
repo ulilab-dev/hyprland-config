8d683136e8d5a1dbeacb7c23d964050bccd8e640
ulilab-dev <ullia5733@gmail.com>
2026-06-07 20:22:58 +0700

    add clipboard rofi

--- config/hypr/custom/autostart.lua
+++ config/hypr/custom/autostart.lua
@@ -7,9 +7,12 @@
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
 
- hl.on("hyprland.start", function ()
-     hl.exec_cmd("nm-applet")
-     hl.exec_cmd("waybar")
-     hl.exec_cmd("swaybg -m fill -i ~/.config/hypr/wallpapers/tree.png")
-     hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
- end)
+hl.on("hyprland.start", function()
+  hl.exec_cmd("nm-applet")
+  hl.exec_cmd("waybar")
+  hl.exec_cmd("swaybg -m fill -i ~/.config/hypr/wallpapers/tree.png")
+  hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
+
+  -- clipboard
+  hl.exec_cmd("wl-paste --type text --watch cliphist store")
+end)
