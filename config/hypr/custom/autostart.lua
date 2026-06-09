3698e7a615191a576edd3173229b415b596a5603
ulilab-dev <ullia5733@gmail.com>
2026-06-10 02:12:41 +0700

    Update autostart.lua

--- config/hypr/custom/autostart.lua
+++ config/hypr/custom/autostart.lua
@@ -8,7 +8,7 @@
 -- Or execute your favorite apps at launch like this:
 
 hl.on("hyprland.start", function()
-  hl.exec_cmd("nm-applet")
+  --hl.exec_cmd("nm-applet")
   hl.exec_cmd("waybar")
   hl.exec_cmd("waypaper --restore")
   hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
